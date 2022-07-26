921d6e4c1d9a23bf216270ff422bc6ca062d3f85
roynaldi19 <roynaldi19@gmail.com>
2022-07-26 11:29:19 +0700

    Initial commit

---
+++ src/main/kotlin/com/roynaldi19/kotlinrestfullapi/KotlinRestfullApiApplication.kt
@@ -0,0 +1,11 @@
+package com.roynaldi19.kotlinrestfullapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestfullApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestfullApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/roynaldi19/kotlinrestfullapi/KotlinRestfullApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.roynaldi19.kotlinrestfullapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestfullApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
